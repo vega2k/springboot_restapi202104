cf23be2428b815d53de94e3d690842191a9e26e3
vega2k <vega2k@naver.com>
2021-04-22 19:55:20 +0900

    Paging처리 + Link 추가

--- restapi/src/main/java/myboot/vega2k/restapi/events/EventController.java
+++ restapi/src/main/java/myboot/vega2k/restapi/events/EventController.java
@@ -7,11 +7,17 @@ import java.net.URI;
 import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +39,24 @@ public class EventController {
 		this.eventValidator = eventValidator;
 	}
 
+	@GetMapping
+	public ResponseEntity<?> queryEvents(Pageable pageable, PagedResourcesAssembler<Event> assembler) {
+		Page<Event> page = this.eventRepository.findAll(pageable);
+		//PagedModel<EntityModel<Event>> pagedResources = assembler.toModel(page);
+		/*
+		  1. toModel(Page<T> page, org.springframework.hateoas.server.RepresentationModelAssembler<T,R> assembler)
+		  2. RepresentationModelAssembler 는 함수형 인터페이스이다.
+		  3. event -> new EventResource(event)  
+		      => RepresentationModelAssembler 의 D toModel(T) 메서드를 재정의 하는것을 람다식으로 표현한 것이다
+		*/
+		PagedModel<RepresentationModel<EventResource>> pagedResources =
+				assembler.toModel(page, event -> new EventResource(event));
+		
+		return ResponseEntity.ok(pagedResources);
+		
+		//return ResponseEntity.ok(this.eventRepository.findAll(pageable));
+	}
+
 	@PostMapping
 	public ResponseEntity<?> createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
 		// Validation API에서 제공하는 어노테이션을 사용해서 입력항목 검증
@@ -47,31 +71,22 @@ public class EventController {
 
 		Event event = modelMapper.map(eventDto, Event.class);
 
-		//등록하기 전에 free와 offline 값을 set 한다
+		// 등록하기 전에 free와 offline 값을 set 한다
 		event.update();
-		
+
 		Event addEvent = eventRepository.save(event);
 
 		// http://localhost:8087/api/events/10
 		WebMvcLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(addEvent.getId());
 		URI createUri = selfLinkBuilder.toUri();
 		/*
-		 * "_links": {
-	        "query-events": {
-	            "href": "http://localhost:8087/api/events"
-	        },
-	        "self": {
-	            "href": "http://localhost:8087/api/events/11"
-	        },
-	        "update-event": {
-	            "href": "http://localhost:8087/api/events/11"
-	        }
-    	}
+		 * "_links": { "query-events": { "href": "http://localhost:8087/api/events" },
+		 * "self": { "href": "http://localhost:8087/api/events/11" }, "update-event": {
+		 * "href": "http://localhost:8087/api/events/11" } }
 		 */
 		EventResource eventResource = new EventResource(addEvent);
 		eventResource.add(linkTo(EventController.class).withRel("query-events"));
 		eventResource.add(selfLinkBuilder.withRel("update-event"));
-		
 
 		return ResponseEntity.created(createUri).body(eventResource);
 	}
