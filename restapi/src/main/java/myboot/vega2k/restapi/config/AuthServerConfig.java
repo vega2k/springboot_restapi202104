e2ca2ad424157ce5bf0149a8abbdb592bc20f9b8
vega2k <vega2k@naver.com>
2021-04-23 20:15:26 +0900

    access token설정

--- restapi/src/main/java/myboot/vega2k/restapi/config/AuthServerConfig.java
+++ restapi/src/main/java/myboot/vega2k/restapi/config/AuthServerConfig.java
@@ -3,6 +3,7 @@ package myboot.vega2k.restapi.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
@@ -17,5 +18,25 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		security.passwordEncoder(passwordEncoder);
+	}
+	
+	/*
+	 * Access Token에 대한 정보 설정
+	 * inMemory()는 클라이언트 정보를 메모리에 저장한다.
+	 * withClient()는 client_id 값을 설정한다. 
+	 * secret()은 client_secret 값을 설정한다.
+	 * authorizedGrantTypes()는 grant_type 값을 설정 password, refresh_token
+	 * accessTokenValiditySeconds() : Access Token 만료 시간, 600초 10분
+     * refreshTokenValiditySeconds() : Refresh Token 만료 시간 , 3600초 1시간
+	 */
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory()
+		 .withClient("myApp")
+		 .secret(this.passwordEncoder.encode("pass"))
+		 .authorizedGrantTypes("password", "refresh_token")
+		 .scopes("read", "write")
+		 .accessTokenValiditySeconds(10 * 60)
+		 .refreshTokenValiditySeconds(6 * 10 * 60);		
 	}
 }
